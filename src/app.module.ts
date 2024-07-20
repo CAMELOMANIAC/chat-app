84ef53c544c40d6344de8887bb699172492228f6
CAMELOMANIAC <122772515+CAMELOMANIAC@users.noreply.github.com>
2024-07-20 14:51:43 +0000

    웹소켓 통신을 위한 socket.io 설치 및 단체채팅 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatController } from './chat/chat.controller';
+import { EventsGateway } from './chat/chat.gateway';
 
 @Module({
   imports: [],
-  controllers: [ChatController],
-  providers: [],
+  controllers: [ChatController],//라우터쪽은 컨트롤러 배열에
+  providers: [EventsGateway],//소켓 통신은 프로바이더 배열에
 })
 export class AppModule {}
---
+++ src/client-test.js
@@ -0,0 +1,40 @@
+const io = require('socket.io-client');
+
+// 서버 URL로 소켓 연결을 생성합니다.
+const socket = io('http://localhost:3000');
+
+const handleConnect = () => {
+  // 연결이 성공적으로 이루어지면 콘솔에 메시지를 출력합니다.
+  socket.on('connect', () => {
+    console.log('서버에 연결되었습니다.');
+  });
+}
+// 서버에 'message' 이벤트를 발생시킵니다.
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+  socket.emit('message', input);
+});
+
+// 서버로부터 'message' 이벤트를 수신하면 콘솔에 메시지를 출력합니다.
+socket.on('messageToClient', (data) => {
+  console.log('서버로부터 메시지를 받았습니다:', data);
+});
+
+//연결되었는지 콘솔에 띄어주고 재시도
+let connectCount = 0;
+const countTimer = setInterval(() => {
+  if (socket.connected === false) {
+    handleConnect()
+    console.log('서버와 연결이 끊겼습니다.');
+    connectCount++;
+  } else {
+    socket.emit('join','room1')
+    clearInterval(countTimer);
+  }
+
+  if (connectCount>3){
+    clearInterval(countTimer);
+    console.log('서버와 연결할 수 없습니다.');
+  }
+
+}, 1000);
