bc62c1edd142b8ed9e96446403727bf34682bc29
yugyusang <yugyusang12@gmail.com>
2024-08-01 23:43:51 +0900

    폴링대신 이벤트를 기준으로 마우스 위치 메세지를 보내도록 수정중

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -11,7 +11,7 @@ import { Server } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST'],
     credentials: true,
   },
@@ -50,21 +50,20 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
           .emit('messageToClient', `${client.id}님이 채팅방에서 나갔습니다`);
       });
     });
-
-    //저장된 마우스 포지션을 클라이언트에게 보내주도록 하는 타이머
-    setInterval(() => {
-      this.handleSendMousePosition();
-    }, 100);
   }
 
   handleDisconnect(client: any): void {
     this.server.emit('messageToClient', client.id + '님이 로그아웃 하셨습니다');
   }
 
+  /**
+   * 메세지를 받아서 룸에 있는 클라이언트에게 보내줍니다.
+   *
+   * @param {*} client
+   * @param {*} payload
+   */
   @SubscribeMessage('message')
   handleMessage(@ConnectedSocket() client, @MessageBody() payload: any): void {
-    //client.emit('messageToClient',payload)
-
     client.rooms.forEach((room: string) => {
       //this.server.to(room).emit('messageToClient', payload);//server는 게이트웨이 서버 전부
       client.broadcast
@@ -72,14 +71,24 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
         .emit('messageToClient', client.id + '님의 메세지:' + payload); //broadcast속성은 발신자를 제외하고보냄
     });
   }
-  private clients: Map<string, Map<string, any[]>> = new Map();
 
+  /** 클라이언트의 마우스 포지션을 저장하는 맵 객체 */
+  private clients: Map<string, Map<string, any[]>> = new Map();
+  private clientsLastPostion: Map<string, any[]> = new Map();
+  timer: NodeJS.Timeout | null = null;
+
+  /**
+   * 마우스 포지션을 받아서 clients 맵객체에 저장합니다.
+   *
+   * @param {*} client - 클라이언트 객체
+   * @param {*} payload - 클라이언트가 보낸 마우스 위치 정보가 담긴 객체
+   */
   @SubscribeMessage('mousePosition')
   handleMousePosition(
     @ConnectedSocket() client,
     @MessageBody() payload: any,
   ): void {
-    const roomName = Array.from(client.rooms)[1]; //룸에 0번은 자기 자신이고 실제 룸은 하나밖에 없도록 했으므로 1번을 가져옴
+    const roomName = Array.from(client.rooms)[1]; //룸에 0번은 자기 자신이고 클라이언트 룸은 단 하나만 join 하도록 했으므로 1번이 실제 위치한 유일한 룸이름
     if (!roomName || typeof roomName !== 'string') {
       return;
     }
@@ -100,27 +109,58 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
     // 업데이트된 배열을 다시 Map 객체에 설정합니다.
     this.clients.set(roomName, currentRoom);
-    //console.log(this.clients);
-  }
 
-  // 클라이언트에게 마우스 위치를 보내주는 타이머의 콜백 메서드
-  handleSendMousePosition() {
     const rooms = this.findAllRooms();
-    rooms.forEach((room, roomName) => {
-      //console.log('clientSize2', this.clients.get(roomName)?.size);
+    rooms.forEach((room) => {
+      //룸에 있는 클라이언트들의 마지막 마우스 위치를 clientsLastPostion에 저장합니다.(클라이언트가 새 마우스 위치정보를 안보낼경우 대신 이걸 보내기위함)
+      const mapToObj = Object.fromEntries(this.clients.get(roomName));
+      room.forEach((clientId) => {
+        if (mapToObj[clientId]) {
+          const lastPostion = [
+            mapToObj[clientId][mapToObj[clientId].length - 1],
+          ];
+          this.clientsLastPostion.set(clientId, lastPostion);
+        }
+      });
+
+      //만약 룸에 있는 클라이언트 수와 클라이언트 맵 객체에 저장된 클라이언트 수가 같다면 즉시 마우스 위치를 보냅니다.
       if (room?.size === this.clients.get(roomName)?.size) {
-        // this.clients 맵 객체를 순환합니다.
-        room.forEach((clientId) => {
-          this.clients.forEach((roomMap) => {
-            //console.log('roomName', roomName, 'clientId', clientId);
-            this.server
-              .to(clientId)
-              .emit('mousePostionToClient', Array.from(roomMap.entries()));
-          });
-        });
-        this.clients.clear();
+        this.handleSendMousePosition();
+        //console.log('sendimmediately');
+      } else if (!this.timer) {
+        //최초 메세지를 받은 이후 메세지가 모이지 않으면 다른 메세지를 대기하도록 타이머를 설정합니다.
+        this.timer = setTimeout(() => {
+          this.handleSendMousePosition();
+          //console.log('sendafter');
+          this.timer = null;
+        }, 200);
       }
     });
+  }
+
+  /**각 클라이언트에게 마우스 위치를 보내주는 메서드 */
+  handleSendMousePosition() {
+    const rooms = this.findAllRooms();
+    rooms.forEach((room) => {
+      room.forEach((clientId) => {
+        this.clients.forEach((roomMap) => {
+          const clientsLastPostionToObject = Object.fromEntries(
+            this.clientsLastPostion,
+          );
+          const roomMapToObject = Object.fromEntries(roomMap);
+          console.log(
+            'clientsLastPostionToObject',
+            clientsLastPostionToObject,
+            '\nroomMapToObject',
+            roomMapToObject,
+          );
+          this.server
+            .to(clientId)
+            .emit('mousePostionToClient', Array.from(roomMap.entries()));
+        });
+      });
+    });
+    this.clients.clear();
   }
 
   /**
